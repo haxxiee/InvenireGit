bdcd525ff62a0f66b7eb7254eb82b4388ffb0744
Linus Foxell <linusfoxell@hotmail.com>
2022-09-07 18:57:59 +0200

    fix: styling fixes, transparent header

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -5,20 +5,22 @@ const title: string = "InvenireGit";
 
 const Navbar = (): JSX.Element => {
   return (
-    <nav className="flex justify-between items-center bg-neutral-100 p-5">
-      <div className="text-2xl ml-2 font-bold">{title}</div>
-      <ul className="flex mx-auto mr-2 [&>*]:px-4">
-        <li>
-          <Link href="/">
-            <a className="mx-2 font-bold">HOME</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <a className="mx-2 font-bold">ABOUT</a>
-          </Link>
-        </li>
-      </ul>
+    <nav className="fixed w-full top-0 left-0 z-10 bg-transparent">
+      <div className="flex justify-between items-center p-5">
+        <div className="text-2xl ml-2 font-bold">{title}</div>
+        <ul className="flex mx-auto mr-2 [&>*]:px-4">
+          <li>
+            <Link href="/">
+              <a className="mx-2 font-bold">HOME</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a className="mx-2 font-bold">ABOUT</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
