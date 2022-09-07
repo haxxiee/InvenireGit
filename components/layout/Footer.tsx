97ae980f4fd11fceb993827ea22962e9faa63efd
Linus Foxell <linusfoxell@hotmail.com>
2022-09-07 22:48:16 +0200

    fix: footer bg-color

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,7 +4,7 @@ const footerYear: number = new Date().getFullYear();
 
 const Footer = (): JSX.Element => {
   return (
-    <footer className="flex justify-center items-center bg-neutral-100 p-5">
+    <footer className="flex justify-center items-center p-5 bg-transparent">
       <p className="text-sm">
         Copyright &copy; {footerYear} All rights reserved
       </p>
