cc7b6cbda9a9c05505d331358be6453b89fda007
Linus Foxell <linusfoxell@hotmail.com>
2022-09-07 18:15:51 +0200

    add: 404 page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,11 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <main className="flex justify-center items-center my-auto">
+    <main className="flex flex-col justify-center items-center my-auto">
       <h3 className="text-3xl">InvenireGit</h3>
+      <div className="flex flex-col">
+        <h3>Welcome to a home built Github finder project!</h3>
+      </div>
     </main>
   );
 };
