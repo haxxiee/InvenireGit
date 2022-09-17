0a234733475142c247d416b488fa8dbb11658a9e
Linus Foxell <linusfoxell@hotmail.com>
2022-09-17 18:11:59 +0200

    add: formevent instead of clickevent

--- components/users/UserSearch.tsx
+++ components/users/UserSearch.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState, MouseEvent } from "react";
+import { ChangeEvent, useState, FormEvent } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { getUsers } from "../../app/store/actions/userAction";
 import { removeAllUsers } from "../../app/store/slices/userSlice";
@@ -12,7 +12,7 @@ const UserSearch = (): JSX.Element => {
   const handleChange = (e: ChangeEvent<HTMLInputElement>) =>
     setText(e.target.value);
 
-  const handleSubmit = (e: MouseEvent<HTMLButtonElement>) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(text);
 
@@ -31,33 +31,34 @@ const UserSearch = (): JSX.Element => {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="container flex justify-center items-center px-4 sm:px-6 lg:px-8 mt-4">
-        <input
-          type="text"
-          className="h-14 w-96 pr-8 pl-5 rounded z-0 shadow-md focus:outline-none dark:bg-gray-800"
-          placeholder="Search profile..."
-          value={text}
-          onChange={handleChange}
-        />
-
-        <button
-          className="py-4 px-4 hover:text-indigo-600"
-          onClick={handleSubmit}
-        >
-          <svg
-            aria-hidden="true"
-            data-prefix="fas"
-            data-icon="search"
-            className="w-6"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="h-14 w-96 pr-8 pl-5 rounded z-0 shadow-md focus:outline-none dark:bg-gray-800"
+            placeholder="Search profile..."
+            value={text}
+            onChange={handleChange}
+          />
+          <button
+            className="py-4 px-4 hover:text-indigo-600"
+            onClick={handleSubmit}
           >
-            <path
-              fill="currentColor"
-              d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
-            ></path>
-          </svg>
-        </button>
+            <svg
+              aria-hidden="true"
+              data-prefix="fas"
+              data-icon="search"
+              className="w-6"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                fill="currentColor"
+                d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
+              ></path>
+            </svg>
+          </button>
+        </form>
       </div>
       {users.length > 1 && (
         <button
